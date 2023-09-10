be1320259eaed52eaf4816500bbd6332da6dbfea
Iván Álvarez Fernández <124284729+ivanaf03@users.noreply.github.com>
2023-09-10 12:16:15 +0200

    Add files via upload

---
+++ funciones/main.py
@@ -0,0 +1,17 @@
+# Declaración
+def funcion():
+    print("Buenos dias")
+
+
+def suma(n1, n2):
+    print(n1 + n2)
+
+
+def suma_return(n1, n2):
+    return n1+n2
+
+
+# Llamada
+funcion()
+suma(1, 2)
+print(suma_return(10, 5))
---
+++ primerosPasos/main.py
@@ -0,0 +1,27 @@
+print("Hola mundo")
+
+print("Con ';' se separan dos expresiones en la misma línea"); print("Pero es una mala práctica")
+
+# Esto es un comentario
+
+nombre = "Iván"  # Esto es una variable
+
+print(nombre)
+
+a = 0
+
+for i in range(5):
+    a += 1
+    print(a)
+
+print(type(nombre)) # type muestra el tipo de la variable
+print(type(a))
+
+numero1=5
+numero2=7
+
+if(numero1>numero2):
+    print("El numero 2 es mayor")
+else:
+    print("El numero 1 es mayor")
+
